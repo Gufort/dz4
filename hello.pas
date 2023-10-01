016b63cf0ad9c85359cd2154488efb72a7d3da74
Игорь Аведисьян <avedisian@sfedu.ru>
2023-10-01 15:12:46 +0300

    Файлик новый

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿﻿begin
+  var n := readinteger('Введите время: ');
+  assert((n>=0) and (n<24));
+  if (n>=4) and (n<=10) then
+    print('Доброе утро, мир!');
+  if (n>=11) and (n<=16) then
+    print('Добрый день, мир!');
+  if (n>=17) and (n<=22) then
+    print('Добрый вечер, мир!')
+  else
+    print('Доброй ночи, мир!');
+end.
